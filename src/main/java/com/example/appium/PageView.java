abe45b6a0a0871aef3c2be78037e157902ac998a
Oleh Morozov <morozov89oleg@gmail.com>
2023-11-19 20:56:24 +0200

    Tests added

--- src/main/java/com/example/appium/PageView.java
+++ src/main/java/com/example/appium/PageView.java
@@ -1,29 +1,30 @@
 package com.example.appium;
 
 import io.appium.java_client.AppiumDriver;
-import io.appium.java_client.MobileElement;
 import io.appium.java_client.pagefactory.AndroidFindBy;
 import io.appium.java_client.pagefactory.AppiumFieldDecorator;
 import io.appium.java_client.pagefactory.iOSXCUITFindBy;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.PageFactory;
 
 public class PageView {
-    AppiumDriver<MobileElement> driver;
+    AppiumDriver driver;
 
-    // TODO define textField MobileElement using @FindBy kind of annotations for iOS and Android
+    @AndroidFindBy(id = "edit")
+    @iOSXCUITFindBy(iOSNsPredicate = "name == 'IntegerA'")
+    private WebElement textField;
 
-    public PageView(AppiumDriver<MobileElement> driver) {
+    public PageView(AppiumDriver driver) {
         this.driver = driver;
         PageFactory.initElements(new AppiumFieldDecorator(driver), this);
     }
 
     public String getTextField() {
-        // TODO return text from the textField element
-        return "";
+        return textField.getText();
     }
 
     public PageView setTextField(String text) {
-        // TODO set text to the textField element
+        textField.sendKeys(text);
         return this;
     }
 }
--- src/test/java/com/example/appium/SampleAppTest.java
+++ src/test/java/com/example/appium/SampleAppTest.java
@@ -1,61 +1,60 @@
 package com.example.appium;
 
 import io.appium.java_client.AppiumDriver;
-import io.appium.java_client.MobileElement;
-import io.appium.java_client.android.Activity;
 import io.appium.java_client.android.AndroidDriver;
 import io.appium.java_client.ios.IOSDriver;
 import io.appium.java_client.remote.MobileCapabilityType;
 import io.appium.java_client.service.local.AppiumDriverLocalService;
-import io.appium.java_client.service.local.AppiumServiceBuilder;
 import org.openqa.selenium.remote.DesiredCapabilities;
+import org.testng.Assert;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
+import java.net.MalformedURLException;
+import java.net.URL;
+
 import static java.lang.System.getenv;
-import static org.testng.Assert.assertEquals;
 
 public class SampleAppTest {
     private AppiumDriverLocalService server;
-    private AppiumDriver<MobileElement> driver;
+    private AppiumDriver driver;
 
     @BeforeClass
-    private void setUp() {
-        DesiredCapabilities capabilities = new DesiredCapabilities();
+    private void setUp() throws MalformedURLException {
+
         String platform = getenv("APPIUM_DRIVER");
         platform = platform == null ? "ANDROID" : platform.toUpperCase();
         String path = System.getProperty("user.dir");
+        server = AppiumDriverLocalService.buildDefaultService();
+        server.start();
 
+
+        DesiredCapabilities capabilities = new DesiredCapabilities();
         if (platform.equals("ANDROID")) {
             capabilities.setCapability(MobileCapabilityType.PLATFORM_NAME, "Android");
-            capabilities.setCapability(MobileCapabilityType.DEVICE_NAME, "PUT_YOUR_DEVICE_NAME");
+            capabilities.setCapability(MobileCapabilityType.DEVICE_NAME, "Pixel 3a");
             capabilities.setCapability(MobileCapabilityType.APP, path + "/ApiDemos-debug.apk");
-
-            server = new AppiumServiceBuilder().usingAnyFreePort().build();
-            server.start();
-            driver = new AndroidDriver<>(server, capabilities);
-
-            ((AndroidDriver<MobileElement>) driver).startActivity(new Activity("io.appium.android.apis", ".view.TextFields"));
+            capabilities.setCapability(MobileCapabilityType.AUTOMATION_NAME,"UIAutomator2");
+            capabilities.setCapability("avd","Pixel_3a_API_33_arm64-v8a");
+            capabilities.setCapability("appActivity", ".view.TextFields");
+            driver = new AndroidDriver(new URL("http://127.0.0.1:4723/"), capabilities);
         } else {
             capabilities.setCapability(MobileCapabilityType.PLATFORM_NAME, "iOS");
-            capabilities.setCapability(MobileCapabilityType.PLATFORM_VERSION, "PUT_YOUR_XCODE_VERSION_HERE");
+            capabilities.setCapability(MobileCapabilityType.PLATFORM_VERSION, "17.0");
             capabilities.setCapability(MobileCapabilityType.AUTOMATION_NAME, "XCuiTest");
-            capabilities.setCapability(MobileCapabilityType.DEVICE_NAME, "PUT_YOUR_DEVICE_NAME_HERE");
-            capabilities.setCapability(MobileCapabilityType.UDID, "PUT_YOUR_DEVICE_UDID_HERE");
+            capabilities.setCapability(MobileCapabilityType.DEVICE_NAME, "iPhone 15 Pro Max");
             capabilities.setCapability(MobileCapabilityType.APP, path + "/TestApp.app.zip");
-
-            server = new AppiumServiceBuilder().usingAnyFreePort().build();
-            server.start();
-            driver = new IOSDriver<>(server, capabilities);
+            capabilities.setCapability( MobileCapabilityType.AUTOMATION_NAME, "XCUITest");
+            driver = new IOSDriver(new URL("http://127.0.0.1:4723/"), capabilities);
         }
     }
 
     @Test
     public void textFieldTest() {
-        // TODO initialise PageView and set "text" to its textField
-
-        // TODO assert that textField equals to "text"
+        PageView view = new PageView(driver);
+        view.setTextField("test");
+        Assert.assertEquals(view.getTextField(), "test", "Text field was not set.");
     }
 
     @AfterClass
